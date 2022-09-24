7e15548a7d6899fe1a900cd5faa03437a0ed9bf2
Park ChanJong <7000cj@gmail.com>
2022-09-25 00:22:42 +0900

    update delete func

--- mydict/mydict.go
+++ mydict/mydict.go
@@ -4,8 +4,12 @@ import "errors"
 
 type Dictionary map[string]string
 
-var errNotFound = errors.New("Not Found")
-var errWordExists = errors.New("That word already exists")
+var (
+	errNotFound   = errors.New("Not Found")
+	errCantUpdate = errors.New("Cant update non-existing word")
+	errWordExists = errors.New("That word already exists")
+)
+
 
 func (d Dictionary) Search(word string) (string, error) {
 	value, exists := d[word]
@@ -24,4 +28,19 @@ func (d Dictionary) Add(word, def string) error {
 		return errWordExists
 	}
 	return nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+	switch err {
+	case nil:
+		d[word] = definition
+	case errNotFound:
+		return errCantUpdate
+	}
+	return nil
+}
+
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
 }
