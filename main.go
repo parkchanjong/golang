8914df30d3c2b2069d0281a900345738072ef80f
Park ChanJong <7000cj@gmail.com>
2022-10-05 22:47:53 +0900

    url checker

--- main.go
+++ main.go
@@ -1,22 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
-
+import "fmt"
 func main() {
-	c := make(chan bool)
-	people := [5]string{"park", "jong", "lee"}
-	for _, person := range people {
-		go isPerson(person, c)
-	}
-	for i := 0; i < len(people); i++ {
-		fmt.Println(<-c)
-	}
-}
-
-func isPerson(person string, c chan bool) {
-	time.Sleep(time.Second * 5)
-	c <- true
+	fmt.Println("hello world")
 }
