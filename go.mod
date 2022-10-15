9d8609cab176682f24b6cde278b70d745e4bf07b
Park ChanJong <7000cj@gmail.com>
2022-10-16 00:26:40 +0900

    echo server init

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.19
 require (
 	github.com/PuerkitoBio/goquery v1.8.0 // indirect
 	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/labstack/echo v3.3.10+incompatible // indirect
 	github.com/labstack/echo/v4 v4.9.1 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.11 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "golang/scrapper"
+import (
+	"golang/scrapper"
+	"os"
+	"strings"
+
+	"github.com/labstack/echo"
+)
+
+const fileName string = "jobs.csv"
+
+func handleHome(c echo.Context) error {
+	return c.File("home.html")
+}
+
+func handleScrape(c echo.Context) error {
+	defer os.Remove("jobs.csv")
+	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
+	scrapper.Scrape(term)
+	return c.Attachment("jobs.csv", "jobs.csv")
+}
 
 func main() {
-	scrapper.Scrape("term")
+	e := echo.New()
+	e.GET("/", handleHome)
+	e.POST("/scrape", handleScrape)
+	e.Logger.Fatal(e.Start(":1323"))
 }
